b87340739ece8548de5d99c6104c6fb876182761
xi.wei <weixuerensoft@gmail.com>
2017-07-24 17:20:21 +0800

    Initial commit

---
+++ src/main/java/com/stream/netty/Server.java
@@ -0,0 +1,54 @@
+package com.stream.netty;
+
+import com.stream.netty.handle.HeartBeatServerHandler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by xi.wei on 2017/7/24.
+ */
+public class Server {
+    
+    private static final Logger logger = LoggerFactory.getLogger(Server.class);
+    
+    private static final int PORT = 9898;
+    
+    public void start() {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap sbs = new ServerBootstrap().group(bossGroup, workerGroup)
+                                          .channel(NioServerSocketChannel.class)
+                                          .localAddress(PORT).childHandler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline().addLast(new IdleStateHandler(5, 0, 0, TimeUnit.SECONDS));
+                            ch.pipeline().addLast("decoder", new StringDecoder());
+                            ch.pipeline().addLast("encoder", new StringEncoder());
+                            ch.pipeline().addLast(new HeartBeatServerHandler());
+                        }
+                    }).option(ChannelOption.SO_BACKLOG, 128).childOption(ChannelOption.SO_KEEPALIVE, true);
+            // 绑定端口，开始接收进来的连接
+            ChannelFuture future = sbs.bind(PORT).sync();
+            future.channel().closeFuture().sync();
+        } catch (Exception e) {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/stream/netty/Test.java
@@ -0,0 +1,13 @@
+package com.stream.netty;
+
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by xi.wei on 2017/7/24.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("123");
+        LoggerFactory.getLogger(Test.class).info("123");
+    }
+}
