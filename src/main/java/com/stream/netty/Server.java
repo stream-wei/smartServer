bccb9092973f85d75b732ece168f9caff1e3f339
xi.wei <weixuerensoft@gmail.com>
2017-07-25 16:41:10 +0800

    o

--- src/main/java/com/stream/netty/Server.java
+++ src/main/java/com/stream/netty/Server.java
@@ -1,6 +1,6 @@
 package com.stream.netty;
 
-import com.stream.netty.handle.DecodeHandler;
+import com.stream.netty.codec.LengthFieldBasedFrameDecoder;
 import com.stream.netty.handle.HeartBeatServerHandler;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
@@ -10,15 +10,10 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
-import io.netty.handler.codec.string.StringDecoder;
-import io.netty.handler.codec.string.StringEncoder;
-import io.netty.handler.logging.LogLevel;
-import io.netty.handler.logging.LoggingHandler;
 import io.netty.handler.timeout.IdleStateHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.net.InetSocketAddress;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -38,7 +33,7 @@ public class Server {
                                           .channel(NioServerSocketChannel.class)
                                           .localAddress(PORT).childHandler(new ChannelInitializer<SocketChannel>() {
                         protected void initChannel(SocketChannel ch) throws Exception {
-                            ch.pipeline().addLast("decoder", new DecodeHandler(4,0,4));
+                            ch.pipeline().addLast("decoder", new LengthFieldBasedFrameDecoder(Integer.MAX_VALUE, 0, 4));
                             ch.pipeline().addLast(new IdleStateHandler(5, 0, 0, TimeUnit.SECONDS));
                             ch.pipeline().addLast(new HeartBeatServerHandler());
                         }
